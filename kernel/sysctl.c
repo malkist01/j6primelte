5f0511e85727a9c23ced57dc80483ea3f7c98a90
geckyn <geckyn@gmail.com>
2020-03-26 00:27:36 +0000

    fs | mm : revert some samsung changes

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -128,9 +128,6 @@ static int __maybe_unused two = 2;
 static int __maybe_unused four = 4;
 static unsigned long one_ul = 1;
 static int one_hundred = 100;
-#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
-static int max_swappiness = 200;
-#endif
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1546,11 +1543,6 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
-		.extra2		= &max_swappiness,
-#else
-		.extra2		= &one_hundred,
-#endif
 	},
 #ifdef CONFIG_HUGETLB_PAGE
 	{
@@ -1626,15 +1618,6 @@ static struct ctl_table vm_table[] = {
 	},
 
 #endif /* CONFIG_COMPACTION */
-#ifdef CONFIG_ZSWAP
-	{
-		.procname	= "zswap_compact",
-		.data		= &sysctl_zswap_compact,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= sysctl_zswap_compaction_handler,
-	},
-#endif
 	{
 		.procname	= "min_free_kbytes",
 		.data		= &min_free_kbytes,
